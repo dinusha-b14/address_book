b8f4d4eedab9c6d58d3cc865ef7b021ba1e60562
dinusha-b14 <dinushab@jobready.com.au>
2016-09-22 21:04:02 +1000

    added the active state for navigation menu items

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def active_state_for_controller(controller)
+    is_active?(controller) ? 'active' : ''
+  end
+
+  def is_active?(controller_name)
+    params[:controller] == controller_name
+  end
 end
